62cc8295538429334539c3d2dbacdea1c0c75995
Thomas <4180596+thomats@users.noreply.github.com>
2023-03-05 13:49:58 +0100

    Build: add DienstleistungenView & styles

--- src/views/DienstleistungenView.vue
+++ src/views/DienstleistungenView.vue
@@ -1,1 +1,58 @@
-<template>Dienstleistungen</template>
+<template>
+  <section class="container-lg">
+    <div class="service__item">
+      <div class="service__img baubegleitung"></div>
+      <div class="service__description">
+        <h2 class="service__header">Baubegleitung</h2>
+        Mit uns haben Sie einen unabhängigen Bauberater an Ihrer Seite, der sich mit dem gesamten
+        Bauprozess, der Bauabnahme und mit Fragen zum Baurecht bestens auskennt und Ihnen mit Rat
+        und mit konkreter Tat zur Seite steht – von den ersten Abklärungen bis zur Bauabnahme.<br />
+        <br />
+        Wir beraten sie Professionell und Kompetent, in allen Arten von neu Bau bis zu Renovationen.
+      </div>
+    </div>
+    <div class="service__item odd">
+      <div class="service__img bauten"></div>
+      <div class="service__description">
+        <h2 class="service__header">Neu & Umbauten</h2>
+        MALEX-BAU erledigt innen alle Aufgaben im Bereich Bau von A bis Z. <br /><br />Wir sind in
+        allen Bereichen Fachmännisch geschult. Wir beraten Sie gerne, wie Ihre Installationen auf
+        den neusten und technischen Stand gebracht werden können. <br /><br />
+        Wir freuen uns mit innen in die Zukunft zu schauen.
+      </div>
+    </div>
+    <div class="service__item">
+      <div class="service__img malerarbeiten"></div>
+      <div class="service__description">
+        <h2 class="service__header">Malerarbeiten</h2>
+        Mit uns haben Sie einen unabhängigen Bauberater an Ihrer Seite, der sich mit dem gesamten
+        Bauprozess, der Bauabnahme und mit Fragen zum Baurecht bestens auskennt und Ihnen mit Rat
+        und mit konkreter Tat zur Seite steht – von den ersten Abklärungen bis zur Bauabnahme.<br />
+        <br />
+        Wir beraten sie Professionell und Kompetent, in allen Arten von neu Bau bis zu Renovationen.
+      </div>
+    </div>
+    <div class="service__item odd">
+      <div class="service__img gipserarbeiten"></div>
+      <div class="service__description">
+        <h2 class="service__header">Gipserarbeiten</h2>
+        Mit uns haben Sie einen unabhängigen Bauberater an Ihrer Seite, der sich mit dem gesamten
+        Bauprozess, der Bauabnahme und mit Fragen zum Baurecht bestens auskennt und Ihnen mit Rat
+        und mit konkreter Tat zur Seite steht – von den ersten Abklärungen bis zur Bauabnahme.<br />
+        <br />
+        Wir beraten sie Professionell und Kompetent, in allen Arten von neu Bau bis zu Renovationen.
+      </div>
+    </div>
+    <div class="service__item">
+      <div class="service__img reinigungen"></div>
+      <div class="service__description">
+        <h2 class="service__header">Reinigung</h2>
+        Mit uns haben Sie einen unabhängigen Bauberater an Ihrer Seite, der sich mit dem gesamten
+        Bauprozess, der Bauabnahme und mit Fragen zum Baurecht bestens auskennt und Ihnen mit Rat
+        und mit konkreter Tat zur Seite steht – von den ersten Abklärungen bis zur Bauabnahme.<br />
+        <br />
+        Wir beraten sie Professionell und Kompetent, in allen Arten von neu Bau bis zu Renovationen.
+      </div>
+    </div>
+  </section>
+</template>
