308ef799667ab77e5d398e83a5e33f9f3dedc0f3
Thomas Lipp <4180596+thomats@users.noreply.github.com>
2023-04-11 15:18:49 +0200

    Fix #5: add Abbrucharbeiten to DienstleistungenView

--- src/views/DienstleistungenView.vue
+++ src/views/DienstleistungenView.vue
@@ -4,57 +4,101 @@
       <div class="service__img baubegleitung"></div>
       <div class="service__description">
         <h2 class="service__header">Baubegleitung</h2>
-        Mit uns erhalten Sie einen unabhängigen Bauberater an Ihrer Seite, der sich hervorragend mit dem gesamten Bauprozess, der Bauabnahme und den Fragen zum Baurecht auskennt. Wir unterstützen Sie mit Rat und tatkräftiger Hilfe – von den ersten Abklärungen bis zur Bauabnahme.
+        Mit uns erhalten Sie einen unabhängigen Bauberater an Ihrer Seite, der
+        sich hervorragend mit dem gesamten Bauprozess, der Bauabnahme und den
+        Fragen zum Baurecht auskennt. Wir unterstützen Sie mit Rat und
+        tatkräftiger Hilfe – von den ersten Abklärungen bis zur Bauabnahme.
         <br />
         <br />
-        Wir beraten Sie professionell und kompetent in allen Bereichen, von Neubauten bis hin zu Renovierungen.
+        Wir beraten Sie professionell und kompetent in allen Bereichen, von
+        Neubauten bis hin zu Renovierungen.
       </div>
     </div>
     <div class="service__item odd">
       <div class="service__img bauten"></div>
       <div class="service__description">
         <h2 class="service__header">Neu & Umbauten</h2>
-        MALEX-BAU übernimmt sämtliche Bauaufgaben von A bis Z. Unsere fachkundig geschulten Mitarbeiter beraten Sie gerne, um Ihre Installationen auf den neuesten technischen Stand zu bringen. <br /><br />
-        Wir freuen uns darauf, gemeinsam mit Ihnen in die Zukunft zu blicken und Ihre Projekte erfolgreich umzusetzen.
+        MALEX-BAU übernimmt sämtliche Bauaufgaben von A bis Z. Unsere fachkundig
+        geschulten Mitarbeiter beraten Sie gerne, um Ihre Installationen auf den
+        neuesten technischen Stand zu bringen. <br /><br />
+        Wir freuen uns darauf, gemeinsam mit Ihnen in die Zukunft zu blicken und
+        Ihre Projekte erfolgreich umzusetzen.
       </div>
     </div>
     <div class="service__item">
       <div class="service__img malerarbeiten"></div>
       <div class="service__description">
         <h2 class="service__header">Malerarbeiten</h2>
-        MALEX-BAU bietet hochwertige Maler- und Gipserarbeiten, Renovierungen, dekorative Farbtechniken, Magnetwände, Leichtbauwände, Fassadenarbeiten und vieles mehr in BS, BL, AG, BE und Umgebung zu fairen Preisen. Wir arbeiten sowohl im Innen- als auch im Außenbereich.
-        <br /><br/>
-        Unsere Experten sind vertraut mit verschiedenen Materialien wie Beton, Stein, Gips, Holz, Kunststoff und Metall. Sie kennen die möglichen Ursachen für Anstrichmängel und bereiten die Oberflächen entsprechend vor, durch Schleifen, Grundieren, Spachteln, Entrosten, Isolieren und Aufhellen.
-        <br /><br/>
-        Wir freuen uns darauf, gemeinsam mit Ihnen Ihre Zukunftspläne zu gestalten.
+        MALEX-BAU bietet hochwertige Maler- und Gipserarbeiten, Renovierungen,
+        dekorative Farbtechniken, Magnetwände, Leichtbauwände, Fassadenarbeiten
+        und vieles mehr in BS, BL, AG, BE und Umgebung zu fairen Preisen. Wir
+        arbeiten sowohl im Innen- als auch im Außenbereich.
+        <br /><br />
+        Unsere Experten sind vertraut mit verschiedenen Materialien wie Beton,
+        Stein, Gips, Holz, Kunststoff und Metall. Sie kennen die möglichen
+        Ursachen für Anstrichmängel und bereiten die Oberflächen entsprechend
+        vor, durch Schleifen, Grundieren, Spachteln, Entrosten, Isolieren und
+        Aufhellen.
+        <br /><br />
+        Wir freuen uns darauf, gemeinsam mit Ihnen Ihre Zukunftspläne zu
+        gestalten.
       </div>
     </div>
     <div class="service__item odd">
       <div class="service__img gipserarbeiten"></div>
       <div class="service__description">
         <h2 class="service__header">Gipserarbeiten</h2>
-        Bei MALEX-BAU bieten wir leidenschaftliches Handwerk für Innen- und Außenputz, Trennwände, abgehängte Decken, Grund- und Deckputze, Akustikdecken, Brandabschottungen sowie Stuckdecken-Restaurierung.<br />
+        Bei MALEX-BAU bieten wir leidenschaftliches Handwerk für Innen- und
+        Außenputz, Trennwände, abgehängte Decken, Grund- und Deckputze,
+        Akustikdecken, Brandabschottungen sowie Stuckdecken-Restaurierung.<br />
         <br />
-        Unsere Gipsermeister gestalten Wände individuell nach Kundenwunsch. Die kreativen Möglichkeiten reichen von dekorativen Spachteltechniken wie Stucco und Lehmputz über Weissputz, Schlämmputz bis hin zu verschiedenen Abriebarten. Wir legen Wert auf ökologische und umweltverträgliche Lösungen.
+        Unsere Gipsermeister gestalten Wände individuell nach Kundenwunsch. Die
+        kreativen Möglichkeiten reichen von dekorativen Spachteltechniken wie
+        Stucco und Lehmputz über Weissputz, Schlämmputz bis hin zu verschiedenen
+        Abriebarten. Wir legen Wert auf ökologische und umweltverträgliche
+        Lösungen.
       </div>
     </div>
     <div class="service__item">
       <div class="service__img boden"></div>
       <div class="service__description">
         <h2 class="service__header">Bodenbeläge</h2>
-        Wir bieten umfassende Bodenbelagsarbeiten wie Parkettschleifen, Versiegelung, Ölen, Beizen, Wachsen, Reinigung, Nachölen und Pflege an. Unsere Experten verlegen und reparieren diverse Bodenbeläge wie Parkett, Laminat, Terrassenböden, Kork, Kautschuk, PVC und Teppich.<br />
+        Wir bieten umfassende Bodenbelagsarbeiten wie Parkettschleifen,
+        Versiegelung, Ölen, Beizen, Wachsen, Reinigung, Nachölen und Pflege an.
+        Unsere Experten verlegen und reparieren diverse Bodenbeläge wie Parkett,
+        Laminat, Terrassenböden, Kork, Kautschuk, PVC und Teppich.<br />
         <br />
-        Ein ansprechendes Ambiente beginnt beim Bodenbelag – vertrauen Sie auf unsere Kompetenz im Innenausbau. Wir sorgen dafür, dass Ihr Zuhause oder Geschäftsräume stilvoll und einladend wirken, indem wir auf Ihre individuellen Bedürfnisse eingehen.
+        Ein ansprechendes Ambiente beginnt beim Bodenbelag – vertrauen Sie auf
+        unsere Kompetenz im Innenausbau. Wir sorgen dafür, dass Ihr Zuhause oder
+        Geschäftsräume stilvoll und einladend wirken, indem wir auf Ihre
+        individuellen Bedürfnisse eingehen.
       </div>
     </div>
     <div class="service__item odd">
+      <div class="service__img abbruch"></div>
+      <div class="service__description">
+        <h2 class="service__header">Abbrucharbeiten</h2>
+        Wir übernehmen Abbrucharbeiten aller Art, von Altbauten bis hin zur
+        fachgerechten Entsorgung von Wohnungselementen. <br /><br />
+        Setzen Sie auf unseren zuverlässigen Service und lassen Sie uns
+        gemeinsam Ihr Projekt erfolgreich umsetzen. <br />
+        <br />Kontaktieren Sie uns noch heute!
+      </div>
+    </div>
+    <div class="service__item">
       <div class="service__img reinigungen"></div>
       <div class="service__description">
         <h2 class="service__header">Reinigung</h2>
-        Als familiengeführtes Bauunternehmen legen wir besonderen Wert auf langfristige Partnerschaften und setzen uns dafür ein, Ihren Immobilienwert durch fachgerechte Reinigung zu erhalten.<br />
+        Als familiengeführtes Bauunternehmen legen wir besonderen Wert auf
+        langfristige Partnerschaften und setzen uns dafür ein, Ihren
+        Immobilienwert durch fachgerechte Reinigung zu erhalten.<br />
         <br />
-        Wir bieten individuell abgestimmte Reinigungs- und Servicekonzepte, geschultes Stammpersonal und feste Ansprechpartner, die Sie jederzeit kompetent informieren. Höchste Qualität, Wirtschaftlichkeit und umweltschonende Reinigungstechniken haben bei uns oberste Priorität.<br/><br/>
-        Wir freuen uns darauf, Ihnen mit unserem engagierten Team zur Seite zu stehen.
+        Wir bieten individuell abgestimmte Reinigungs- und Servicekonzepte,
+        geschultes Stammpersonal und feste Ansprechpartner, die Sie jederzeit
+        kompetent informieren. Höchste Qualität, Wirtschaftlichkeit und
+        umweltschonende Reinigungstechniken haben bei uns oberste Priorität.<br /><br />
+        Wir freuen uns darauf, Ihnen mit unserem engagierten Team zur Seite zu
+        stehen.
       </div>
     </div>
   </section>
